69f75a4d174f953a2797d22795f25055ef8728d6
Hafaykon <ytpmaxz@gmail.com>
2023-11-15 18:13:11 +0100

    Complted API

--- src/api/api_calls.js
+++ src/api/api_calls.js
@@ -1,65 +1,27 @@
+import axios from "axios";
 
-
-export function fetchWeather() {
-//
-}
-/*
-    const lat = 59.91;
-    const lon = 10.75;
+export async function fetchWeather(latitude, longitude) {
     const base_url = "https://api.open-meteo.com/v1/metno";
-    const current_params = "&current=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m"
-    const url2 = "https://api.open-meteo.com/v1/metno?latitude=59.91&longitude=10.75&current=temperature_2m,relative_humidity_2m,precipitation,weather_code,wind_speed_10m"
-
-    const { isPending, error, data } = useQuery({
-        queryKey: ['weather'],
-        queryFn: () =>
-          fetch(base_url + `?latitude=${lat}&longitude=${lon}` + current_params).then(
-            (res) => res.json(),
-          ),
-  })
-    if (isPending) return 'Loading...'
-    if (error) return 'An error has occurred: ' + error.message;
-    return data
+    const current_params = "&current=temperature_2m,precipitation,weather_code,wind_speed_10m&wind_speed_unit=ms"
+     try {
+        const response = await axios.get(base_url + `?latitude=${latitude}&longitude=${longitude}`+ current_params);
+        return response.data;
+    } catch (error) {
+        console.error(`Error fetching weather data for ${latitude}, ${longitude}:`, error);
+        throw error;
+    }
 }
 
-const base_url = "https://api.open-meteo.com/v1/metno\""
-    fetch(base_url).then((res) => res.json());
-    const params = {
-        "latitude": 59.91,
-        "longitude": 10.75,
-        "current": ["temperature_2m", "relative_humidity_2m", "precipitation", "weather_code", "wind_speed_10m"]
-    };
-    const url = "https://api.open-meteo.com/v1/metno";
-    const responses = await fetchWeatherApi(url, params);
-
-    // Helper function to form time ranges
-    const range = (start, stop, step) =>
-        Array.from({length: (stop - start) / step}, (_, i) => start + i * step);
-
-    // Process first location. Add a for-loop for multiple locations or weather models
-    const response = responses[0];
-
-    // Attributes for timezone and location
-    const utcOffsetSeconds = response.utcOffsetSeconds();
-    const timezone = response.timezone();
-    const timezoneAbbreviation = response.timezoneAbbreviation();
-    const latitude = response.latitude();
-    const longitude = response.longitude();
-
-    const current = response.current();
-
-    // Note: The order of weather variables in the URL query and the indices below need to match!
-    return {
-        current: {
-            time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
-            temperature2m: current.variables(0).value(),
-            relativeHumidity2m: current.variables(1).value(),
-            precipitation: current.variables(2).value(),
-            weatherCode: current.variables(3).value(),
-            windSpeed10m: current.variables(4).value(),
-        },
-
-    };
+export async function fetchCoordinates(city) {
+    const base_url = `https://api.api-ninjas.com/v1/geocoding?city=${city}`
+    try {
+        const response = await axios.get(base_url,{
+             headers: { 'X-Api-Key': "Z7Pf91RtpwAyS046DnfiOZi503IIbsbjNRCJxTvP" }
+        });
+        console.log(response.data[0].latitude, response.data[0].longitude);
+        return response.data[0];
+    } catch (error) {
+        console.error(`Error fetching geocoding data for ${city}`);
+        throw error;
+    }
 }
-*/
-
--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,43 +1,50 @@
 import React, {useEffect} from "react";
 import "../style/global.css";
+import "../style/WeatherCard.css";
 import {useQuery} from "@tanstack/react-query";
 import{Link} from "react-router-dom";
+import {fetchCoordinates, fetchWeather} from "../api/api_calls";
 
-export const WeatherCard = ({latitude, longitude}) => {
 
-    const lat = 59.91;
-    const lon = 10.75;
-    const base_url = "https://api.open-meteo.com/v1/metno";
-    const current_params = "&current=temperature_2m,precipitation,weather_code,wind_speed_10m"
+export const WeatherCard = ({city, latitude, longitude}) => {
 
-    const { isPending, error, data } = useQuery({
-        queryKey: ['weather'],
-        queryFn: () =>
-          fetch(base_url + `?latitude=${lat}&longitude=${lon}` + current_params).then(
-            (res) => res.json(),
-          ),
-  })
-    if (isPending) return 'Loading...'
-    if (error) return 'An error has occurred: ' + error.message;
+    const { isLoading, error, data } = useQuery({
+        queryKey: ['weather', latitude, longitude],
+        queryFn: () => fetchWeather(latitude,longitude)
+    })
+    if (isLoading) return 'Loading...'
+    if (error) return 'Encountered an error while querying' + error.message;
+
+    const test = fetchCoordinates("Trondheim")
 
     const datePart = data.current.time.split('T')[0];
     const [year, month, day] = datePart.split('-');
     const formattedDate = `${day}/${month}-${year}`;
 
-    return(
-        <>
-            <Link to="/location">
-                <div className="weather-container">
-                    <h2>Været</h2>
-                    <div className="content-container">
-                        <div>{data.current.temperature_2m} °C</div>
-                        <div>{data.current.weather_code} *CodE*</div>
-                        <div>{data.current.precipitation} mm</div>
-                        <div>{data.current.wind_speed_10m} km/t</div>
-                        <div>{formattedDate}</div>
+
+    return (
+        <Link to="/location">
+            <div className="weather-container">
+                <h2>{city} {formattedDate}</h2>
+                <div className="weather-content-container">
+                    <div>
+                        <img className='weatherIcon' src="/icons-search.svg" alt="Weather Icon"/>
+                        <p>{data.current.temperature_2m} °C</p>
+                    </div>
+                    <div>
+                        <img className='weatherIcon' src="/icons-search.svg" alt="Weather Icon"/>
+                        <p>{data.current.weather_code} *CodE*</p>
+                    </div>
+                    <div>
+                        <img className='weatherIcon' src="/icons-search.svg" alt="Weather Icon"/>
+                        <p>{data.current.precipitation} mm</p>
+                    </div>
+                    <div>
+                        <img className='weatherIcon' src="/icons-search.svg" alt="Weather Icon"/>
+                        <p>{data.current.wind_speed_10m} m/s</p>
                     </div>
                 </div>
-            </Link>
-        </>
-    )
+            </div>
+        </Link>
+    );
 }
